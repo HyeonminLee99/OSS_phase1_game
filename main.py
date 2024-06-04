ab91f7442228678eb49cab4ec53b311050d8055c
Hyeonminlee73 <dlgusals002@gmail.com>
2024-06-05 00:50:08 +0900

    check collision

--- main.py
+++ main.py
@@ -20,6 +20,11 @@ screen = pygame.display.set_mode((cell_size*cols,cell_size*rows))
 pygame.display.set_caption("Let's Tetris")
 block_size = 30
 
+cur_block = None
+next_block = None
+game_over = False
+paused = False
+
 blocks = {
     'I' : [(0,1) , (1,1) , (2,1) , (3,1)],
     'O' : [(1,0) , (2,0) , (1,1) , (2,1)],
@@ -57,7 +62,43 @@ def remove_row(board , row) :
     del board[row]
     return [[0 for i in range(cols)]] + board
 
+def new_stone() :
+    if cur_block is None :
+        cur_block = blocks[rand(len(blocks))]
+        next_block = blocks[rand(len(blocks))]
 
+    cur_block = next_block
+    next_block = blocks[rand(len(blocks))]
+
+
+def check_collision(board, block , offset) :
+    off_x , off_y = offset
+    for cy, row in enumerate(blcok) :
+        for cx, cell in enumerate(row) :
+            try:
+                if cell and board[cy+off_y][cx+off_x] :
+                    return True
+            except IndexError:
+                return True
+    return False
+"""
+def move(board,step) :
+    if not game_over and not paused :
+        new_x = cur_block + step
+
+        if new_x < 0 :
+            new_x = 0
+        if new_x > cols - len(cur_block) :
+            new_x = cols - len(cur_block)
+        if not check_collision(board , )
+
+"""
+
+    
+
+
+
+pygame.time.set_timer(pygame.USEREVENT +1 , 1000)
 rotated_blocks = {k: rotate_clockwise(v) for k,v in blocks.items()}
 running = True
 
