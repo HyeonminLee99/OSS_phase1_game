a3e26da90eb11b1ab83a4be35c9fe53d01bb8042
Hyeonminlee73 <dlgusals002@gmail.com>
2024-06-04 20:48:09 +0900

    screen setting + draw_block

--- main.py
+++ main.py
@@ -11,6 +11,11 @@ ORANGE = (255,165,0)
 CYAN = (0,255,255)
 PURPLE = (128,0,128)
 
+
+screen = pygame.display.set_mode((400,600))
+pygame.display.set_caption("Let's Tetris")
+block_size = 30
+
 blocks = {
     'I' : [(0,1) , (1,1) , (2,1) , (3,1)],
     'O' : [(1,0) , (2,0) , (1,1) , (2,1)],
@@ -32,3 +37,9 @@ colors = {
     'J' : CYAN,
     'L' : PURPLE
 }
+
+def draw_block(block_type , block_position , offset_x , offset_y) :
+    for pos in block_position :
+        x = pos[0] * block_size + offset_x
+        y = pos[1] * block_size + offset_y
+        pygame.draw.rect(screen , colors[block_type], (x,y,block_size ,block_size))
